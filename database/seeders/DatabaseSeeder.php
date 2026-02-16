9246fb03d759f1ca27ee1bdf8d3ff156fe723252
Jeffri Alpian <jeffrialpian@192.168.1.6>
2026-02-16 21:03:22 +0700

    mengahapus folder lang

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+       public function run(): void
     {
         DB::table('users')->insert([
             'username' => 'admin',
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
@@ -17,7 +17,7 @@ export default defineConfig({
         //     host: '192.168.1.6' // GANTI dengan IP Laptop/PC Anda yang Anda pakai tadi
         // },
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
